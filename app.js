bbc3a0de58000134f20186b6d7fd353d4c56de23
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-04-19 17:38:03 +0000

    created domain generator, prints in console

---
+++ app.js
@@ -0,0 +1,11 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+
+pronoun.forEach(first => 
+        adj.forEach ( second =>
+            noun.forEach(third => 
+                console.log(first + second  + third + ".com")
+                )
+            )
+    ) 
