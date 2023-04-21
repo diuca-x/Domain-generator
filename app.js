ffee8e6c79b0632c487b9b9891743612563a7e1e
diuca-x <126406046+diuca-x@users.noreply.github.com>
2023-04-21 14:57:22 +0000

    created a domain generator in src to generate a random domain on reload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var pronoun = ['the','our'];
-var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
+import "bootstrap";
+import "./style.css";
 
-pronoun.forEach(first => 
-        adj.forEach ( second =>
-            noun.forEach(third => 
-                console.log(first + second  + third + ".com")
-                )
-            )
-    ) 
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+pronoun.forEach((first) =>
+  adj.forEach((second) =>
+    noun.forEach((third) => console.log(first + second + third + ".com"))
+  )
+);
--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  document.getElementById("domain").innerHTML =
+    pronoun[pickRand(pronoun)] +
+    adj[pickRand(adj)] +
+    noun[pickRand(noun)] +
+    ".com";
+};
+
+const pickRand = arr => {
+  return Math.floor(Math.random() * arr.length);
 };
